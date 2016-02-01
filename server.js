05fe82a20cc57f58b81d7bb373decad904e91f40
Ravi Kumar Yadav <ravi.k.yadav108@gmail.com>
2016-02-01 15:21:37 +0530

    basic http server which serves 'hello world' as response body for each request

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js holds the code for servering HTTP requests
+
+var http = require('http');
+var port = 3456;
+
+function onRequest(request, response){
+	// for each request following code will be executed twice
+	// due to subsequent call for "favicon.ico" file
+	console.log("Request recieved");
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello World!!!");
+	response.end();
+}
+
+http.createServer(onRequest).listen(port);
+
+console.log("Server started at port " + port + "...");
+
