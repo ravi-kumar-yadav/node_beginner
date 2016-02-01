1e69b6f2e3a47b300963e7291f60da4bf8dd851d
Ravi Kumar Yadav <ravi.k.yadav108@gmail.com>
2016-02-01 15:39:05 +0530

    created module for 'server.js' by modifying its code and using exports command

--- server.js
+++ server.js
@@ -3,16 +3,20 @@
 var http = require('http');
 var port = 3456;
 
-function onRequest(request, response){
-	// for each request following code will be executed twice
-	// due to subsequent call for "favicon.ico" file
-	console.log("Request recieved");
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write("Hello World!!!");
-	response.end();
+function start() {
+	function onRequest(request, response){
+		// for each request following code will be executed twice
+		// due to subsequent call for "favicon.ico" file
+		console.log("Request recieved");
+		response.writeHead(200, {"Content-Type":"text/plain"});
+		response.write("Hello World!!!");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(port);
+
+	console.log("Server started at port " + port + "...");
 }
 
-http.createServer(onRequest).listen(port);
-
-console.log("Server started at port " + port + "...");
+exports.start = start;
 
